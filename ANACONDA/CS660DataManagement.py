c85d3da25b10594ed3bae31805866edad222ccde
Stan Rosenbaum <ForeverTangent@gmail.com>
2017-11-21 21:38:48 -0500

    Back to Safe

--- ANACONDA/CS660DataManagement.py
+++ ANACONDA/CS660DataManagement.py
@@ -478,17 +478,50 @@ def getSubDirectoryAndEXTForDataFlavor( dataFlavor ):
     
     return { 'SubDir' : subCombinedDataDir, 'EXT': fileExtention }
         
-    
+
 
 def load_dataset( nameID, dataFlavor ):
-    print("FILL IN")
-    xTrainLearningSet = None
-    yTrainVerificationSet = None
-    xTestLearningSet = None
-    yTestVerificationSet = None
-    return xTrainLearningSet, yTrainVerificationSet, xTestLearningSet, yTestVerificationSet
+    """
+    Unpickle the Training and Test, Learning and Verification Arrays for a session.
+
+    PARAMETERS:
+    nameID: String of the set ID.
+    dataFlavor: String of dataFlavor ['ICOLOR', 'IDEPTH', 'PCLOUD']
+    """
+    # Just in case an Int get passed in.
+    nameID = str(nameID)
+    
+    trainingLearningPickleFileName = os.path.join( 
+            pickleDataDir, 
+            ('TRAINING_' + nameID + '_LEARNING_' + dataFlavor + '_npArray.pkl') 
+            )
+    trainingVerificationPickleFileName = os.path.join( 
+            pickleDataDir, 
+            ('TRAINING_' + nameID + '_VERIFICATION_' + dataFlavor + '_npArray.pkl') 
+            )
+    testLearningPickleFileName = os.path.join( 
+            pickleDataDir, ('TEST_' + nameID + '_LEARNING_' + dataFlavor + '_npArray.pkl') 
+            )
+    testVerificationPickleFileName = os.path.join( 
+            pickleDataDir, ('TEST_' + nameID + '_VERIFICATION_' + dataFlavor + '_npArray.pkl') 
+            )
+    
+    
+    trainingLearningSet = pickle.load( open( trainingLearningPickleFileName, 'rb' ) )
+    trainingVerificationSet = pickle.load( open( trainingVerificationPickleFileName, 'rb' ) )
+    testLearningSet = pickle.load( open( testLearningPickleFileName, 'rb' ) )
+    testVerificationSet = pickle.load( open( testVerificationPickleFileName, 'rb' ) )
+    
+    return  ( 
+            trainingLearningSet,
+            trainingVerificationSet,
+            testLearningSet,
+            testVerificationSet
+            )
 
 
+
+    
 def pickNormOrFlopForTrainingTestingLists( theList ):
     """
     Add '1' or '0' to pick a norm or flop version of picture.
